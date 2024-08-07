81705fbec28bcf0ee52c8ec354e2a797e486a491
Eya Maamouri <92765964+eya144@users.noreply.github.com>
2024-08-07 19:53:23 +0400

    Initial commit

---
+++ shop.php
@@ -0,0 +1,86 @@
+<?php
+
+session_start();
+
+include("includes/db.php");
+include("includes/header.php");
+include("functions/functions.php");
+include("includes/main.php");
+
+?>
+  <!-- MAIN -->
+  <main>
+    <!-- HERO -->
+    <div class="nero">
+      <div class="nero__heading">
+        <span class="nero__bold">shop</span> AT AVE
+      </div>
+      <p class="nero__text">
+      </p>
+    </div>
+  </main>
+
+
+<div id="content" ><!-- content Starts -->
+<div class="container" ><!-- container Starts -->
+
+
+
+
+
+<div class="col-md-12" ><!-- col-md-9 Starts --->
+
+
+<?php getProducts(); ?>
+
+</div><!-- row Ends -->
+
+<center><!-- center Starts -->
+
+<ul class="pagination" ><!-- pagination Starts -->
+
+<?php getPaginator(); ?>
+
+</ul><!-- pagination Ends -->
+
+</center><!-- center Ends -->
+
+
+
+</div><!-- col-md-9 Ends --->
+
+
+
+</div><!--- wait Ends -->
+
+</div><!-- container Ends -->
+</div><!-- content Ends -->
+
+
+
+<?php
+
+include("includes/footer.php");
+
+?>
+
+<script src="js/jquery.min.js"> </script>
+
+<script src="js/bootstrap.min.js"></script>
+
+<script>
+
+
+
+
+
+
+
+
+
+
+</script>
+
+</body>
+
+</html>
